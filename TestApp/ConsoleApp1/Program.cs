d8ffda5dc90cd78d11a8ee74ea4f7475a44a76d3
Andres <a0z9@rambler.ru>
2022-10-11 13:30:50 +0300

    init commit test Debugger

---
+++ TestApp/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using static System.Console;
+using static andestech.learning2022.krasn.utils.Utils;
+
+namespace andestech.learning2022.krasn
+{
+    internal class Program
+    {
+
+        static void Greets() {
+            WriteLine("Hello All!");
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine("Hi all, learning is UP!");
+            WriteLine("Salute, learning is UP!");
+            Greets();
+            Greets2();
+            WriteLine("Howdoyoudo, learning is UP!");
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ TestApp/ConsoleApp1/Utils.cs
@@ -0,0 +1,14 @@
+﻿
+using static System.Console;
+
+namespace andestech.learning2022.krasn.utils
+{
+    internal static class Utils
+    {
+        public static void Greets2() {
+            WriteLine("Hello from Greets class!!");
+
+        }
+
+    }
+}
